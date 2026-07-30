1ac048db813dcc266031837894934d4f9572420b
alvindimas05 <alvin.dimas.praditya@gmail.com>
2026-07-30 18:02:26 +0700

    refactor: split into components and add prettier

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,13 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 declare module "*.css" {
-  const content: any;
-  export default content;
+    const content: any;
+    export default content;
 }
 declare module "aos";
 declare module "bootstrap/dist/js/bootstrap.bundle.min.js";
 declare module "swiper/bundle";
 
 interface Window {
-  AOS: any;
-  Swiper: any;
+    AOS: any;
+    Swiper: any;
 }
